b4abf4cfe34e170a485a9f8e1b4de33b8c1e8827
Dominik Nowak <dnowak94@live.de>
2024-06-03 18:47:09 +0200

    fix /projects route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { ProjectListComponent } from './projects/project-list/project-list.component';
 
 export const routes: Routes = [
-    { path:'', component: ProjectListComponent}
+    { path:'', component: ProjectListComponent },
+    { path: 'projects', component: ProjectListComponent }
 ];
