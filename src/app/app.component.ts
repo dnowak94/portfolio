4ad1163ef18563867a5c709f76d55d762090eaa8
Dominik Nowak <dnowak94@live.de>
2024-06-03 18:46:40 +0200

    added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProjectsModule } from './projects/projects.module';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProjectsModule],
+  imports: [CommonModule, RouterOutlet, ProjectsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
